f16369615405a90287da2797b3bcbfbcb95926b2
Peter <zhouycmail@gmail.com>
2025-12-15 09:23:07 +0800

    修改注释

--- SISO/AWGNChannel.m
+++ SISO/AWGNChannel.m
@@ -1,5 +1,6 @@
 function y = AWGNChannel(u, noiseVar )
 %% Initialization
+%% Initialization2
 persistent AWGN
 if isempty(AWGN)
     AWGN             = comm.AWGNChannel('NoiseMethod', 'Variance', ...
